df6d605c3d9c1c2474d29f5bcf74a6de25243d59
chorin11 <ha.iy.r8ao@gmail.com>
2020-11-10 21:42:37 +0900

    add#5

--- main.php
+++ main.php
@@ -1,4 +1,9 @@
 <?php
 
-echo 'Hello taguchi' . PHP_EOL;
-echo 'Hi taguchi' . PHP_EOL;
+$name = 'taguchi';
+
+// echo 'Hello taguchi' . PHP_EOL;
+// echo 'Hi taguchi' . PHP_EOL;
+
+echo 'Hello ' . $name . PHP_EOL;
+echo 'Hi ' . $name . PHP_EOL;
