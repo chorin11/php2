14fd5b4e429fba2c33a2dda4f846c50b3bcb5b98
chorin11 <ha.iy.r8ao@gmail.com>
2020-11-11 22:47:30 +0900

    add#13

--- main.php
+++ main.php
@@ -1,19 +1,11 @@
 <?php
 
-$a = 'hello';
-$b = 10;
-$c = -1.3;
-$d = null;
-$e = true;
+$score = 40;
 
-// var_dump($a);
-// var_dump($b);
-// var_dump($c);
-// var_dump($d);
-// var_dump($e);
-
-$a = (float)10;
-$b = (string)1.3;
-
-var_dump($a);
-var_dump($b);
+if ($score >= 80) {
+  echo 'Great!' . PHP_EOL;
+} elseif ($score >= 60) {
+  echo 'Good!' . PHP_EOL;
+} else {
+  echo 'OK!' .PHP_EOL;
+}
