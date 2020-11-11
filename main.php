7f82aea8b8e8cd220f6de60dc65f6d93fe15fded
chorin11 <ha.iy.r8ao@gmail.com>
2020-11-11 22:33:07 +0900

    add#11

--- main.php
+++ main.php
@@ -1,12 +1,19 @@
 <?php
 
-// $name = 'taguchi';
-// $name ='dotinstall';
+$a = 'hello';
+$b = 10;
+$c = -1.3;
+$d = null;
+$e = true;
 
-// define('NAME', 'taguchi');
-// define('NAME', 'dotinstall');
-// echo NAME . PHP_EOL;
+// var_dump($a);
+// var_dump($b);
+// var_dump($c);
+// var_dump($d);
+// var_dump($e);
 
-const NAME = 'taguchi';
-const NAME = 'dotinstall';
-echo NAME . PHP_EOL;
+$a = (float)10;
+$b = (string)1.3;
+
+var_dump($a);
+var_dump($b);
