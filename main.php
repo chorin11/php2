cda3ee483dacf1d8eea6457e963e2f70f2c294e4
chorin11 <ha.iy.r8ao@gmail.com>
2020-11-15 19:47:35 +0900

    add#21

--- main.php
+++ main.php
@@ -1,8 +1,16 @@
 <?php
 
-for ($i =1; $i <= 10; $i++) {
-  if ( $i === 4 ) {
-    break;
-  }
-  echo $i . PHP_EOL;
+function showAd() 
+{
+  echo '----------' . PHP_EOL;
+  echo '--- Ad ---' . PHP_EOL;
+  echo '----------' . PHP_EOL;
 }
+
+showAd();
+echo 'Tom is great!' . PHP_EOL;
+echo 'Bob is great!' . PHP_EOL;
+showAd();
+echo 'Steve is great!' . PHP_EOL;
+echo 'Bob is great!' . PHP_EOL;
+showAd();
