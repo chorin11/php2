5a8aed1ec42a8722e4c34c61c4be951e33bcd578
chorin11 <ha.iy.r8ao@gmail.com>
2020-11-19 22:40:37 +0900

    add#30

--- main.php
+++ main.php
@@ -1,10 +1,12 @@
 <?php
 
 $scores = [
-  90,
-  40,
-  100,
+  'first' => 90,
+  'second' => 40,
+  'third' => 100,
 ];
 
-$scores[1] = 60;
-echo $scores[1] . PHP_EOL;
+// var_dump($scores);
+// print_r($scores);
+
+echo $scores['third'] . PHP_EOL;
