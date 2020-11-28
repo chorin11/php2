716fdc3b04ddf6a0eeee06a038f1a364cbc1aa88
chorin11 <ha.iy.r8ao@gmail.com>
2020-11-28 17:01:41 +0900

    add#4

--- main.php
+++ main.php
@@ -1,4 +1,10 @@
 <?php
 
-echo 'Hello taguchi' . PHP_EOL;
+$name = 'dotinstall';
+
+// echo 'Hello taguchi' . PHP_EOL;
 // echo 'Hi taguchi' . PHP_EOL;
+
+echo 'Hello ' . $name . PHP_EOL;
+// echo 'Hi ' . $name . PHP_EOL;
+echo 'Hi ' . $Name . PHP_EOL;
